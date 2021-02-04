c667ee79a61204eaf55faa52c64589eb64022614
Agmar Torres <agmartorres@gmail.com>
2021-02-03 21:58:38 -0300

    input: teste input values with static css

---
+++ cypress/integration/input-form.spec.js
@@ -0,0 +1,13 @@
+describe("Input form ", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    it("focuses input on load", () => {
+      cy.focused().should("have.class", "new-todo");
+    });
+
+    it.only("accepts input", () => {
+      const typedText = "text";
+      cy.get(".new-todo").type(typedText).should("have.value", typedText);
+    });
+  });
+});
